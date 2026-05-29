bc2bb99780db0a8c4ed8268bebd7f0989de28ae1
Maximiliano Duran <duranmaximiliano90@hotmail.com>
2026-05-29 20:16:43 -0300

    Agregamos comentarios

--- trifecta_duranmaximiliano.py
+++ trifecta_duranmaximiliano.py
@@ -6,7 +6,7 @@ def main():
         numero = int(entrada)
         
         # Verificamos si es distinto de cero
-        if numero != 0:
+        if numero != 0:#es opcional ponerlo aca o dentro de cada intento
             print(f"¡Número {numero} ingresado! El programa ha iniciado.")
             # Aquí va el resto de tu código principal
         else:
@@ -16,5 +16,5 @@ def main():
         # Esto captura el error si ingresan texto, decimales o caracteres especiales
         print("Entrada inválida. No es un número entero. El programa finalizará.")
 
-if __name__ == "__main__":
+if __name__ == "__main__":#ajustar la condicion dependiendo de donde esta el contenido
     main()
