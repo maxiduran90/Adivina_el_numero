af6bef7871f0b48f2bcb4c73b409f0bf08018c23
Maximiliano Duran <duranmaximiliano90@hotmail.com>
2026-05-29 22:49:21 -0300

    Entrega Final

--- trifecta_duranmaximiliano.py
+++ trifecta_duranmaximiliano.py
@@ -6,7 +6,57 @@ def main():
         numero = int(entrada)
         
         # Verificamos si es distinto de cero
-        if numero != 0:#es opcional ponerlo aca o dentro de cada intento
+        if numero != 0:
+            print(f"¡Número {numero} ingresado! El programa ha iniciado.")
+            # Aquí va el resto de tu código principal
+        else:
+            print("Se ingresó un 0. El programa finalizará.")
+            
+    except ValueError:
+        # Esto captura el error si ingresan texto, decimales o caracteres especiales
+        print("Entrada inválida. No es un número entero. El programa finalizará.")
+        
+if __name__ == "__main__":
+    main()
+
+
+
+# Solicitamos la palabra o frase al usuario
+texto = input("Ingresa una palabra o frase: ")
+# Contamos los caracteres con la función len()
+cantidad_caracteres = len(texto)
+
+# Mostramos el resultado en pantalla
+print(f"La cantidad de caracteres es: {cantidad_caracteres}")
+
+
+
+import math
+
+n = 9  # Reemplaza esto con el número que necesites
+
+# Calcular el factorial
+factorial = math.factorial(n)
+
+# Comprobar si es par o impar
+if factorial % 2 == 0:
+    resultado_paridad = "par"
+else:
+    resultado_paridad = "impar"
+
+print(f"El factorial de {n} es {factorial}, y es un número {resultado_paridad}.")
+
+
+
+def main():
+    entrada = input("Ingresa un número entero: ")
+    
+    try:
+        # Intentamos convertir la entrada a un número entero
+        numero = int(entrada)
+        
+        # Verificamos si es distinto de cero
+        if numero != 0:
             print(f"¡Número {numero} ingresado! El programa ha iniciado.")
             # Aquí va el resto de tu código principal
         else:
@@ -16,5 +66,4 @@ def main():
         # Esto captura el error si ingresan texto, decimales o caracteres especiales
         print("Entrada inválida. No es un número entero. El programa finalizará.")
 
-if __name__ == "__main__":#ajustar la condicion dependiendo de donde esta el contenido
-    main()
+if __name__ == "__main__":    main()
